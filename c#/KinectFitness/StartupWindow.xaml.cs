988d26ab2de1090a531e85700b7d5c818c94a147
brthiess <brthiess@ualberta.ca>
2014-03-03 23:29:40 -0700

    Initial UI Files

---
+++ c#/KinectFitness/StartupWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinectFitness
+{
+    /// <summary>
+    /// Interaction logic for StartupWindow.xaml
+    /// </summary>
+    public partial class StartupWindow : Page
+    {
+        public StartupWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Play(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            KinectWindow kw = new KinectWindow();
+            this.NavigationService.Navigate(kw);
+
+        }
+
+        private void Button_Options(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
